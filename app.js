b66b85df48d50c2dfecf249452c6ef11a72ae202
Lucas Cardoso <lucas.v.cardoso@gmail.com>
2025-02-09 15:16:52 -0300

    Voltando para o estado anterior

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de cadastro de jogos");
 console.log("Aplicação está se ocnectando ao banco de dados");
-//Mais um console log de teste
-console.log("O Paulo fez esse commit sem você ver na aula!");
-console.log("Enviando para o GitHub");
